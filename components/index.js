a612b85d70c43290c2aa792c99af13302ccc264b
David <davidmwathi412@gmail.com>
2025-03-02 19:30:18 +0300

    Added changes

---
+++ components/index.js
@@ -0,0 +1,29 @@
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+let xStart = null;
+let yStart = null;
+
+function handleTouchStart(evt) {
+  const firstTouch = evt.touches[0];
+  xStart = firstTouch.clientX;
+  yStart = firstTouch.clientY;
+}
+
+function handleTouchMove(evt) {
+  if (!xStart || !yStart) {
+    return;
+  }
+
+  const xMove = evt.touches[0].clientX;
+  const yMove = evt.touches[0].clientY;
+
+  const xDiff = xStart - xMove;
+  const yDiff = yStart - yMove;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    evt.preventDefault();
+  }
+
+
+}
